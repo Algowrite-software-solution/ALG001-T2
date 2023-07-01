a62b5b52c66d4f8481d0f93bf20da3395824536e
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-07-01 21:10:19 +0530

    countries added

--- api/addCountry.php
+++ api/addCountry.php
@@ -1,44 +1,16 @@
-
 <?php
+
+
 require("../app/dbQuery.php");
 
-// Read the JSON file
-$jsonData = file_get_contents('../countries.json');
+$countryArray = json_decode(file_get_contents("countries.json"));
 
-// Convert the JSON data to an associative array
-$data = json_decode($jsonData);
-
-
-// Create a new instance of the DB class
 $db = new DB();
 
-foreach ($data as $object) {
-    $names = $object->name;
-    $code = $object->code;
 
-    // Prepare the SQL statement
-    $sql = "INSERT INTO country (`country_name`, `country_code`)  VALUES (?, ?)";
-
-    // Prepare the statement and bind parameters
-    $stmt = $db->prepare($sql, 'ss', [$names, $code]);
-
-    // Check for any errors
-    if (!$stmt) {
-        die("Statement preparation failed: " . $db->error);
-    }
-
-    // Execute the statement
-    $stmt->execute();
-
-    
-
-    // Close the statement
-    $stmt->close();
+foreach ($countryArray as $value) {
+    $db->prepare("INSERT INTO `country`(`country_name`, `country_code`) VALUES (?, ?)", "ss", [$value->name, $value->code]);
+    echo $value->name . "<br>";
 }
 
-
-
-
-
-
-
+echo "success";
--- app/dbQuery.php
+++ app/dbQuery.php
@@ -13,8 +13,8 @@ class DB
         $host = 'localhost';
         $user = 'root';
 
-        $password = 'KaviskaDilshan12#$'; // malidu
-        //$password = 'JanithNirmal12#$'; // janith
+        // $password = 'KaviskaDilshan12#$'; // malidu
+        $password = 'JanithNirmal12#$'; // janith
         $database = 'alg001_db';
 
         $this->host = $host;
