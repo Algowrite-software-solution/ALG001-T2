c618ccd909332d341e87a5dfc756d7d8010a4d3a
kaviska <126580314+kaviska@users.noreply.github.com>
2023-07-13 10:18:16 -0700

    tt
    tt

---
+++ app/response_sender.php
@@ -0,0 +1,18 @@
+<?php 
+
+class response_sender {
+
+    public static function sendJson ($responseObject){
+
+        echo(json_encode($responseObject));
+        exit();
+
+
+    }
+
+}
+
+
+
+?>
+
