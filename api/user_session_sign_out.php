05044dce4598848487c6b5ba6c92b6cdb9fd0d2e
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-08-02 18:23:50 +0530

    updated

--- api/user_session_sign_out.php
+++ api/user_session_sign_out.php
@@ -2,10 +2,8 @@
 
 session_start();
 
-if(isset($_SESSION["alg_user"])){
-
+if (isset($_SESSION["alg_user"])) {
     $_SESSION["alg_user"] = null;
     session_destroy();
-    echo("signedout");
-
+    echo ("signedout");
 }
